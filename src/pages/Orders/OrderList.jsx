140b6ff15c3e80b1b8e65afbe9e656804d416ea2
unknown <muhammedrashidr222@gmail.com>
2025-08-18 18:34:13 +0530

    error fix

--- src/pages/Orders/OrderList.jsx
+++ src/pages/Orders/OrderList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import { URL } from '../api';
 
 const OrderList = () => {
   const navigate = useNavigate();
